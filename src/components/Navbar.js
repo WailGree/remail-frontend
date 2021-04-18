42dd73e020cab5ff1814ea9953b70a0271fc5d5a
vb <verabertalan@gmail.com>
2021-04-18 21:42:09 +0200

    Use existing elements in NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,19 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { useStoreActions, useStoreState } from "easy-peasy";
-import picture from "./pic/logo.png";
 import { logOut } from "../controller/Controller";
 import Link from "../elements/Link";
+import Button from "../elements/Button";
+import Logo from "../elements/Logo";
 
 function Navbar() {
   const changeLoginState = useStoreActions((actions) => actions.toggle);
-  const setMessages = useStoreActions((actions) => actions.setMessages);
   const User = useStoreState((actions) => actions.userName);
 
   function handleSignOut() {
     logOut();
     changeLoginState();
   }
-
-  const Button = styled.button`
-    background: transparent;
-    border-radius: 4px;
-    border: 9px solid red;
-    color: palevioletred;
-    margin: 0 1em;
-    padding: 0.25em 0.5em;
-
-    ${(props) =>
-      props.primary &&
-      css`
-        background: red;
-        color: white;
-      `};
-  `;
-
-  const Logo = styled.div`
-    width: 75px;
-    height: 75px;
-    border-radius: 40px;
-    background-image: url(${picture});
-    -webkit-background-size: cover;
-    background-size: cover;
-  `;
 
   const Nav = styled.div`
     display: flex;
@@ -51,23 +26,6 @@ function Navbar() {
     font-family: fantasy;
   `;
 
-  const Container = styled.div`
-    width: 300px;
-    height: 300px;
-    color: #000;
-    top: 50%;
-    left: 50%;
-    padding: 60px 30px;
-    position: absolute;
-    transform: translate(-50%, -50%);
-    box-sizing: border-box;
-    box-shadow: 8px 8px 50px #000;
-    border-radius: 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  `;
   const logOutButton = (
     <Button primary onClick={handleSignOut}>
       Sign out
